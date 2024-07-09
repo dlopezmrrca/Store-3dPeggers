fadc2950ef72f57a914e91f32ca9b881d841fefa
Daniel <dlopezmaldonado@rrc.ca>
2024-07-09 14:52:01 -0500

    working on cart, it is not working

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,8 @@
+class CartsController < ApplicationController
+
+ def create
+  
+ end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
       get "success", on: :collection
     end
   end
+
+  resources :carts, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
