19b4168195c373e212d9b5111d2da490caea8105
Daniel <dlopezmaldonado@rrc.ca>
2024-07-09 09:21:29 -0500

    buy now is working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :categories
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :products
+  resources :products do
+    resource :buy_now, only: [:show], controller: :buy_now
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
